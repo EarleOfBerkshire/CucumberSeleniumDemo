ec091b8e21f170472142477773d4a7619386f715
EarleOfBerkshire <145764459+EarleOfBerkshire@users.noreply.github.com>
2025-05-15 13:12:14 +0100

    re

--- src/main/java/com/gitlab/richard_earle_uk/stepdefinitions/ProductsSteps.java
+++ src/main/java/com/gitlab/richard_earle_uk/stepdefinitions/ProductsSteps.java
@@ -1,19 +1,17 @@
 package com.gitlab.richard_earle_uk.stepdefinitions;
 
 import com.gitlab.richard_earle_uk.pages.ProductsPage;
-import com.gitlab.richard_earle_uk.utils.WebDriverManagerUtil;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.WebDriver;
 
 public class ProductsSteps {
 
-  private final WebDriver driver;
-  private ProductsPage productsPage;
+  private final ProductsPage productsPage;
 
-  public ProductsSteps(WebDriverManagerUtil webDriverManager) {
-    this.driver = webDriverManager.getDriver();
+  public ProductsSteps(ProductsPage productsPage) {
+    this.productsPage = productsPage;
   }
+
   @When("the user adds the first item to the cart")
   public void theUserAddsTheFirstItemToTheCart() {
     productsPage.addFirstItemToCart();
